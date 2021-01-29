c7610fb4dc62622fd6cd6240c949598de353789f
anhao12138 <1993697020@qq.com>
2021-01-29 17:51:59 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+window.setTimeout(function() {
+
+
+    let startingPoint = 0
+    const header = document.querySelector('.banner')
+
+    header.addEventListener('mouseenter', (e) => {
+        startingPoint = e.clientX
+        header.classList.add('moving')
+    })
+
+    header.addEventListener('mouseout', (e) => {
+        header.classList.remove('moving')
+        header.style.setProperty('--percentage', 0.5)
+    })
+
+    header.addEventListener('mousemove', (e) => {
+        let percentage = ((startingPoint - e.clientX) / window.outerWidth) * 2 + 0.5
+
+        header.style.setProperty('--percentage', percentage)
+    })
+}, 500);
